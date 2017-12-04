941937bd96e70e2ea2c361239ba31ad7fb83bf30
Andrew Phillips <skeledrew@gmail.com>
2017-12-04 12:51:17 -0600

    Initial commit

---
+++ train.py
@@ -0,0 +1,26 @@
+#! /usr/bin/env python3
+
+'''
+
+'''
+
+import common
+
+
+def make_net(**kwargs):
+    ''' Create DNN architecture.
+    '''
+    pass
+
+def train(**kwargs):
+    '''Train a classifier.
+    '''
+    pass
+
+def main():
+    '''Setup training session.
+    '''
+    pass
+
+if __name__ == '__main__':
+    main()
